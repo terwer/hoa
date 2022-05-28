8c277889bb8cdf3586255c8f5ac42acf84df4bdc
terwer <youweics@163.com>
2022-05-28 23:00:23 +0800

    fix:添加前端校验以及重写Action的valivate方法实现后端校验

--- src/main/java/com/terwergreen/hoa/action/user/SaveUserAction.java
+++ src/main/java/com/terwergreen/hoa/action/user/SaveUserAction.java
@@ -4,6 +4,7 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.terwergreen.hoa.bean.User;
 import com.terwergreen.hoa.filter.HoaStartupFilter;
 import com.terwergreen.hoa.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,6 +35,26 @@ public class SaveUserAction extends ActionSupport {
 
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Override
+    public void validate() {
+        if (StringUtils.isEmpty(user.getFirstname())) {
+            this.addFieldError("user.firstname", "姓名不能为空");
+        }
+        if (StringUtils.isEmpty(user.getLastname())) {
+            this.addFieldError("user.lastname", "昵称不能为空");
+        }
+        if (null == user.getAge()) {
+            this.addFieldError("user.age", "年龄不能为空");
+        }
+
+        if (user.getFirstname().length() > 150) {
+            this.addFieldError("user.firstname", "姓名不能超过150字符");
+        }
+        if (user.getLastname().length() > 150) {
+            this.addFieldError("user.lastname", "昵称不能超过150字符");
+        }
     }
 
     @Override
