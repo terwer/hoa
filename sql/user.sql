38af189c44b8ab2bbdeb9cbef43b541fc00d94d9
terwer <youweics@163.com>
2022-06-05 11:18:11 +0800

    fix:修改数据库字符集

--- sql/user.sql
+++ sql/user.sql
@@ -1,7 +1,7 @@
 select version();
 
 -- 创建数据库
--- create database hoa default character set utf8mb4 collate utf8mb4_general_ci;
+-- create database hoa default character set utf8mb4 collate utf8mb4_bin;
 -- use hoa;
 
 -- 用户表
